de71106360c01176ca2bf4e5fed52335a57d4965
ArduinoidIOT <rohroexperiment@gmail.com>
2020-09-24 10:44:04 +0530

    idk what

--- daemon.py
+++ daemon.py
@@ -1,5 +1,6 @@
 from discord.ext.commands import Bot
 from os import environ
+from discord import Message
 from cogs import XKCDCog, MoneyManagerCog
 
 bot = Bot("!")
@@ -8,6 +9,11 @@ bot = Bot("!")
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+
+@bot.event
+async def on_message(msg: Message):
+    m = msg.mentions[0]
+    print(m.desktop_status, m.mobile_status, m.web_status)
 
 
 bot.add_cog(XKCDCog(bot))
